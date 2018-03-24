726b08215b58dcb58b971dc9bc9522302cf6d7cf
katrina <inlove0131@hotmail.com>
2018-03-24 10:21:34 +0800

    add new testcase

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -2,6 +2,7 @@
 using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqTests
 {
@@ -16,19 +17,43 @@ namespace LinqTests
 
             var expected = new List<Product>()
             {
-                //todo
+                new Product{Id=2, Cost=21, Price=210, Supplier="Yahoo" },
+                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
+                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" },
             };
 
-            expected.ToExpectedObject().ShouldEqual(actual);
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void find_products_that_price_between_200_and_500_linq()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.Where(x => x.Price > 200 && x.Price < 500);
+
+            var expected = new List<Product>()
+            {
+                new Product{Id=2, Cost=21, Price=210, Supplier="Yahoo" },
+                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
+                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" },
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
     }
 }
 
 internal class WithoutLinq
 {
-    public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary)
+    public static IEnumerable<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary)
     {
-        throw new System.NotImplementedException();
+        foreach (var product in products)
+        {
+            if (product.Price > lowBoundary && product.Price < highBoundary)
+            {
+                yield return product;
+            }
+        }
     }
 }
 
